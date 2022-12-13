5c8007f38147a3622106ae18a43ff4e198469ff2
오성인 <dhtjddls12@naver.com>
2022-12-13 14:52:03 +0900

    feat: todo 수정 기능 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,54 @@
-import "./App.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import AddTodo from "./components/AddTodo";
 import Todo from "./components/Todo";
 
-function App() {
-  const [TodoItems, setTodoItems] = useState([
+const App = () => {
+  const [todoItems, setTodoItems] = useState([
     {
       id: 1,
-      title: "my todo1",
+      title: "My Todo1",
       done: false,
     },
     {
       id: 2,
-      title: "my todo2",
+      title: "My Todo2",
       done: false,
     },
     {
       id: 3,
-      title: "my todo3",
+      title: "My Todo3",
       done: true,
     },
   ]);
+  const todoId = useRef(4);
+
+  // AddTodo 컴포넌트는 상위 컴포넌트(App)의 todoItems(state)에 접근 불가능
+  // 상위 컴포넌트(App)은 AddTodo 컴포넌트 접근 가능
+  // => App 컴포넌트에 addItem() 함수를 정의하고, 해당 함수를 AddTodo props로 넘겨야 함
+  const addItem = (newItem) => {
+    // newItem - {id: xx, title: xx, done: false}
+    newItem.id = todoId.current++; // key를 위한 id 설정
+    newItem.done = false; // done 초기화
+    // 기존 todoItems를 유지하고, 새로운 newItem을 추가
+    setTodoItems([...todoItems, newItem]); // setTodoItems(todoItems.concat(newItem))
+  };
+
+  // 전체 Todo 리스트(todoItems)는 App 컴포넌트에서 관리하고 있으므로
+  // deleteItem() 함수는 App 컴포넌트에 작성해야 함
+  const deleteItem = (targetItem) => {
+    let newTodoItems = todoItems.filter((item) => item.id !== targetItem.id);
+    setTodoItems(newTodoItems);
+  };
 
   return (
     <div className="App">
-      {TodoItems.map((TodoItem) => {
-        return <Todo TodoItem={TodoItem}></Todo>;
+      <AddTodo addItem={addItem} />
+      {todoItems.map((item) => {
+        // console.log(item); // {id: 1, title: 'My Todo1', done: false}
+        return <Todo key={item.id} item={item} deleteItem={deleteItem} />;
       })}
     </div>
   );
-}
+};
 
 export default App;
--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,18 +1,55 @@
-const Todo = ({ TodoItem }) => {
-  const { id, done, title } = TodoItem;
+import { useState } from "react";
+
+const Todo = ({ item, deleteItem }) => {
+  // console.log(item); // { id: 1, title: 'todo1', done: false, }
+  const { id, title, done } = item;
+  const [todoItem, setTodoItem] = useState(item);
+  const [readOnly, setReadOnly] = useState(true);
+
+  const onDeleteBtnClick = () => {
+    deleteItem(todoItem);
+  };
+
+  // title input 커서가 깜빡인다고 수정이 가능한 것은 아님
+  // 사용자가 키보드 입력할 때마다 todoItem의 title을 새 값으로 변경
+  const editTitleHandler = (e) => {
+    setTodoItem({ ...todoItem, title: e.target.value });
+  };
+
+  const editDoneHandler = (e) => {
+    setTodoItem({ ...todoItem, done: e.target.checked });
+  };
+
+  const clickInput = () => {
+    setReadOnly(false);
+  };
+
+  const onEnterKeyPress = (e) => {
+    if (e.key === "Enter") {
+      setReadOnly(true);
+    }
+  };
+
   return (
     <div className="Todo">
-      <br />
-      <label htmlFor={`todo${id}`}>
-        {title}
-        <input
-          type="checkbox"
-          name={`todo${id}`}
-          id={`todo${id}`}
-          value={title}
-          defaultChecked={done}
-        />
-      </label>
+      <input
+        type="checkbox"
+        id={`todo${id}`}
+        name={`todo${id}`}
+        value={`todo${id}`}
+        defaultChecked={done}
+        onChange={editDoneHandler}
+      />
+      {/* <label htmlFor={`todo${id}`}>{title}</label> */}
+      <input
+        type="text"
+        readOnly={readOnly}
+        onClick={clickInput}
+        value={todoItem.title}
+        onChange={editTitleHandler}
+        onKeyPress={onEnterKeyPress}
+      />
+      <button onClick={onDeleteBtnClick}>DELETE</button>
     </div>
   );
 };
